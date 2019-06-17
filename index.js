acf6cb11c667921e46eb6cce1ea6b0d6bc75f339
Christian Perez <perezchristian1012@gmail.com>
2019-06-17 18:58:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays() {
+return "Happy Holidays"
+}
+function happyHolidaysTo("Mom") {
+return 'Happy holidays, ${Mom}!'
+}
+function happyHolidaysTo('Christmas', "Mom") {
+return 'Happy ${Christmas}, ${Mom}!'
+}
+function holidayCountdown(100, 'Christmas') {
+return 'It\'s ${100} days until ${Christmas}!'
+}
